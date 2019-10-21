2821b4d49bdc3a52268805ea3789b871c050297f
刘邦 <899427412@qq.com>
2019-10-21 22:09:48 +0800

    增加一个输入框

--- TestGit/TestGit/ViewController.swift
+++ TestGit/TestGit/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         let button = UIButton.init(frame: CGRect(x: 50, y: 50, width: 80, height: 80))
         self.view.addSubview(button)
         button.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.blue
+        let textfield = UITextField.init(frame: CGRect(x: 0, y: 0, width: 90, height: 90))
+        self.view.addSubview(textfield)
         // Do any additional setup after loading the view.
     }
 
